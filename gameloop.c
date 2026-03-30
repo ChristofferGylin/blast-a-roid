1f09f70128b3df8978320c17ce23ddab81aae8f4
Christoffer Gylin <christoffer@christoffergylin.com>
2026-03-31 01:52:45 +0200

    Added shipSprite to world

--- gameloop.c
+++ gameloop.c
@@ -3,12 +3,24 @@
 
 void gameLoop(int level) {
 
-    Texture2D shipSprite = LoadTexture("ship.png");
+    Texture2D shipSprite = LoadTexture("./assets/ship.png");
+
+    float shipPosX = (GetScreenWidth() / 2) - (shipSprite.width / 2);
+    float shipPosY = (GetScreenHeight() / 2) - (shipSprite.height / 2);
+    float shipRot = 0;
 
     while(!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(BLACK);
+            DrawTexturePro(
+                shipSprite,
+                (Rectangle){0, 0, shipSprite.width, shipSprite.height},
+                (Rectangle){shipPosX, shipPosY, shipSprite.width, shipSprite.height},
+                (Vector2){ shipSprite.width / 2.0f, shipSprite.height / 2.0f},
+                shipRot,
+                WHITE
+            );
         EndDrawing();
     }
 }
