022eafaa7c8acdd010ff5a8d337d880afa048637
Christoffer Gylin <christoffer@christoffergylin.com>
2026-03-31 03:30:18 +0200

    Changed shipPos from two floats to one Vector2

--- gameloop.c
+++ gameloop.c
@@ -5,8 +5,7 @@ void gameLoop(int level) {
 
     Texture2D shipSprite = LoadTexture("./assets/ship.png");
 
-    float shipPosX = GetScreenWidth() / 2;
-    float shipPosY = GetScreenHeight() / 2;
+    Vector2 shipPos = { GetScreenWidth() / 2, GetScreenHeight() / 2 };
     float shipRot = 0;
 
     while(!WindowShouldClose())
@@ -16,7 +15,7 @@ void gameLoop(int level) {
             DrawTexturePro(
                 shipSprite,
                 (Rectangle){0, 0, shipSprite.width, shipSprite.height},
-                (Rectangle){shipPosX, shipPosY, shipSprite.width, shipSprite.height},
+                (Rectangle){shipPos.x, shipPos.y, shipSprite.width, shipSprite.height},
                 (Vector2){ shipSprite.width / 2.0f, shipSprite.height / 2.0f},
                 shipRot,
                 WHITE
