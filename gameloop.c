11e013c0e868572da581f15495781c724348bea4
Christoffer Gylin <christoffer@christoffergylin.com>
2026-03-31 21:45:14 +0200

    Made out of bounds logic

--- gameloop.c
+++ gameloop.c
@@ -6,10 +6,13 @@
 void gameLoop(int level) {
 
     const float ROTATION_SPEED = 150.0f;
+    const float THRUST_FACTOR = 100.0f;
+    const float MAX_VELOCITY = 100.0f;
 
     Texture2D shipSprite = LoadTexture("./assets/ship.png");
 
     Vector2 shipPosition = { GetScreenWidth() / 2, GetScreenHeight() / 2 };
+    Vector2 shipVelocity = {0,0};
     float shipRotation = 0.0f;
 
     while(!WindowShouldClose())
@@ -26,8 +29,25 @@ void gameLoop(int level) {
 
         shipRotation = fmodf(shipRotation, 360.0f);
 
-        if (shipRotation < 0.0f) {
+        if (shipRotation < 0.0f)
+        {
             shipRotation += 360.0f;
+        }
+
+        if (shipPosition.x < 0.0f - shipSprite.width)
+        {
+            shipPosition.x = GetScreenWidth() + shipSprite.width;
+        } else if (shipPosition.x > GetScreenWidth() + shipSprite.width)
+        {
+            shipPosition.x = 0.0f - shipSprite.width; 
+        }
+
+        if (shipPosition.y < 0.0f - shipSprite.height)
+        {
+            shipPosition.y = GetScreenHeight() + shipSprite.height;
+        } else if (shipPosition.y > GetScreenHeight() + shipSprite.height)
+        {
+            shipPosition.y = 0.0f - shipSprite.height; 
         }
 
         BeginDrawing();
