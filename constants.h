71e06727656d77ed558c95d43aa0e97404f0f656
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-01 21:23:16 +0200

    Added #ifndef check to constants.h

--- constants.h
+++ constants.h
@@ -1,3 +1,8 @@
-    const float ROTATION_SPEED = 270.0f;
-    const float THRUST_FACTOR = 2.5f;
-    const float MAX_VELOCITY = 300.0f;
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const float ROTATION_SPEED = 270.0f;
+const float THRUST_FACTOR = 2.5f;
+const float MAX_VELOCITY = 300.0f;
+
+#endif
