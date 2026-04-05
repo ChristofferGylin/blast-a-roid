b1339d309a21782655bcc1340a47d65a314baa98
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-05 19:26:56 +0200

    Created structs for ShotPoolObject, ShotObjectPool, and changed created in Shot struct to destroyTime

--- shooting.h
+++ shooting.h
@@ -1,10 +1,21 @@
 #include "raylib.h"
 #include <stdbool.h>
 
+#define MAX_SHOTS 1024
+
 typedef struct {    
     Vector2 posistion;
     Vector2 direction;
-    double created;
+    double destroyTime;
     bool destroyed;
 }Shot;
 
+typedef struct {
+    bool active;
+    Shot shot;
+}ShotPoolObject;
+
+typedef struct {
+    ShotPoolObject shots[MAX_SHOTS];
+    int activeCount;
+}ShotObjectPool;
