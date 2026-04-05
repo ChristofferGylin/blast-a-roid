fe6b9d1ed8e9b6f1831f037e1c1d6a11959a117c
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-06 01:47:49 +0200

    Created constants for asteroid score levels and used in addScore

--- player.c
+++ player.c
@@ -1,8 +1,9 @@
 #include "player.h"
 #include "asteroid.h"
+#include "constants.h"
 
 void addScore(Player* player, Asteroid* ast) {
-    int score = ast->level == 1 ? 50 : ast->level == 2 ? 100 : 300;
+    int score = ast->level == 1 ? ASTEROID_POINTS_1 : ast->level == 2 ? ASTEROID_POINTS_2 : ASTEROID_POINTS_3;
     player->score += score;
     player->levelBonus += score * player->timeBonusMultiplier;
 }
