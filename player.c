500ddf8544b6ab4198041549dc13303b16427f52
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-06 02:45:40 +0200

    Added temporary printf of scores for debugging

--- player.c
+++ player.c
@@ -1,11 +1,16 @@
 #include "player.h"
 #include "asteroid.h"
 #include "constants.h"
-
+#include <stdio.h>
+#include <inttypes.h>
 void addScore(Player* player, Asteroid* ast) {
     int score = ast->level == 1 ? ASTEROID_POINTS_1 : ast->level == 2 ? ASTEROID_POINTS_2 : ASTEROID_POINTS_3;
     player->score += score;
     player->levelBonus += score * player->timeBonusMultiplier;
+
+    printf("Score: %" PRIu64 "\n", player->score);
+    printf("Level Bonus: %" PRIu64 "\n", player->levelBonus);
+    printf("Time Bonus Multiplier: %d\n", player->timeBonusMultiplier);
 }
 
 void initPlayer(Player* player) {
