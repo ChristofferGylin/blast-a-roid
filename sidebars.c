28268f28710ffaa9104795b2247b11ef92599e70
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-07 02:29:32 +0200

    Created renderStats function and used to render score, level bonus and level number in sidebar

--- sidebars.c
+++ sidebars.c
@@ -9,42 +9,58 @@ Color topColor = {0, 25, 38, 255};
 Color bottomColor = {0, 13, 36, 255};
 Color lineColor = {156, 192, 255, 128};
 
-int topOffset = 20;
-int titleOffset = 35;
-int sideOffset = 20;
-int blockOffset = 100;
-int fontSize = 22;
-int fontSpacing = 4;
+int renderStats(uint64_t value, char title[], int startY) {
+    
+    int sideOffset = 20;
+    int fontSize = 22;
+    int fontSpacing = 4;
+    int gap = 12;
+    int underlineGap = 2;
+
+    char valueText[32];
+    snprintf(valueText, sizeof(valueText), "%" PRIu64, value);
+
+    Vector2 valueSize = MeasureTextEx(GetFontDefault(), valueText, fontSize, fontSpacing);
+    Vector2 titleSize = MeasureTextEx(GetFontDefault(), title, fontSize, fontSpacing);
+
+    Vector2 valuePosition = {
+        (SIDEBAR_WIDTH - sideOffset) - valueSize.x,
+        startY + titleSize.y + gap
+    };
+
+    Vector2 titlePosition = {
+        SIDEBAR_WIDTH / 2,
+        startY
+    };
+
+    Vector2 titleOrigin = {
+        titleSize.x / 2,
+        0
+    };
+
+    Vector2 valueOrigin = {0, 0};
+
+    float underlineY = titlePosition.y + titleSize.y + underlineGap;
+    float underLineStartX = titlePosition.x - (titleSize.x / 2);
+    float underLineEndX = titlePosition.x + (titleSize.x / 2);
+
+    DrawTextPro(GetFontDefault(), title, titlePosition, titleOrigin, 0, fontSize, fontSpacing, RAYWHITE);
+    DrawLine(underLineStartX, underlineY, underLineEndX, underlineY, RAYWHITE);
+    DrawTextPro(GetFontDefault(), valueText, valuePosition, valueOrigin, 0, fontSize, fontSpacing, RAYWHITE);
+
+    return valuePosition.y + valueSize.y;
+}
 
 void renderSidebars(Player* player) {
-
-    
-    char scoreTitle[] = "SCORE";
-    char bonusTitle[] = "BONUS";
-    char levelTitle[] = "LEVEL";
-    char scoreValue[32];
-    char bonusValue[32];
-    char levelValue[12];
-
-    snprintf(scoreValue, sizeof(scoreValue), "%" PRIu64, player->score);
-    snprintf(bonusValue, sizeof(bonusValue), "%" PRIu64, player->levelBonus);
-    snprintf(levelValue, sizeof(levelValue), "%03d", player->level);
-
-    Vector2 scoreSize = MeasureTextEx(GetFontDefault(), scoreValue, fontSize, fontSpacing);
-    Vector2 scoreTitleSize = MeasureTextEx(GetFontDefault(), scoreTitle, fontSize, fontSpacing);
-    float scoreX = (SIDEBAR_WIDTH - sideOffset) - scoreSize.x;
-
-    Vector2 bonusSize = MeasureTextEx(GetFontDefault(), bonusValue, fontSize, fontSpacing);
-    float bonusX = (SIDEBAR_WIDTH - sideOffset) - bonusSize.x;
+    int startY = 20;
+    int statsBlockGap = 25;
 
     DrawRectangleGradientV(0, 0, SIDEBAR_WIDTH, SCREEN_HEIGHT, topColor, bottomColor);
     DrawRectangleGradientV(SCREEN_WIDTH - SIDEBAR_WIDTH, 0, SIDEBAR_WIDTH, SCREEN_HEIGHT, topColor, bottomColor);
     DrawLine(SIDEBAR_WIDTH, 0, SIDEBAR_WIDTH, SCREEN_HEIGHT, lineColor);
     DrawLine(SCREEN_WIDTH - SIDEBAR_WIDTH, 0, SCREEN_WIDTH - SIDEBAR_WIDTH, SCREEN_HEIGHT, lineColor);
 
-    int scoreTitleOffset = (SIDEBAR_WIDTH - scoreTitleSize.x) / 2;
-
-    DrawTextPro(GetFontDefault(), scoreTitle, (Vector2){x: SIDEBAR_WIDTH / 2, y: topOffset}, (Vector2){x: scoreTitleSize.x / 2, y: 0}, 0, fontSize, fontSpacing, RAYWHITE);
-    DrawLine(scoreTitleOffset, topOffset + scoreTitleSize.y + 2, SIDEBAR_WIDTH - scoreTitleOffset, topOffset + scoreTitleSize.y + 2, RAYWHITE);
-    DrawTextPro(GetFontDefault(), scoreValue, (Vector2){x: scoreX, y: topOffset + titleOffset}, (Vector2){x: 0, y: 0}, 0, fontSize, fontSpacing, RAYWHITE);
+    startY = renderStats(player->score, "SCORE", startY) + statsBlockGap;
+    startY = renderStats(player->levelBonus, "BONUS", startY) + statsBlockGap;
+    startY = renderStats((uint64_t)player->level, "LEVEL", startY) + statsBlockGap;   
 }
--- sidebars.h
+++ sidebars.h
@@ -1,8 +1,11 @@
 #ifndef SIDEBARS_H
 #define SIDEBARS_H
 
+#include <inttypes.h>
+
 typedef struct Player Player;
 
 void renderSidebars(Player* player);
+int renderStats(uint64_t value, char title[], int startY);
 
 #endif
