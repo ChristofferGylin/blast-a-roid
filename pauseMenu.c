bff0b7daf27e3eddb74dde256e8b35a9c070f88e
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-14 01:13:37 +0200

    created drawPauseMenuBackground fn

--- pauseMenu.c
+++ pauseMenu.c
@@ -1,1 +1,32 @@
 #include "pauseMenu.h"
+#include "raylib.h"
+#include "constants.h"
+#include <stdbool.h>
+
+void drawLayoutContainers() {
+
+    int menuWidth = 400;
+    int menuHeight = 500;
+
+    Rectangle background = {
+        0,
+        0,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT
+    };
+
+    Rectangle menuContainer = {
+        (SCREEN_WIDTH / 2) - (menuWidth / 2),
+        (SCREEN_HEIGHT / 2) - (menuHeight / 2),
+        menuWidth,
+        menuHeight
+    };
+
+    Vector2 origin = {0, 0};
+
+    Color backgroundColor = Fade(BLACK, 0.6f);
+    Color menuColor = Fade(DARKBLUE, 0.6f);
+
+    DrawRectanglePro(background, origin, 0, backgroundColor);
+    DrawRectanglePro(menuContainer, origin, 0, menuColor);
+}
