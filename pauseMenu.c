ac225269469df83488605d579c67549be2d24696
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-14 03:38:01 +0200

    Created initPausMenu fn, changed drawPausMenuBackground and created new structs

--- pauseMenu.c
+++ pauseMenu.c
@@ -2,31 +2,70 @@
 #include "raylib.h"
 #include "constants.h"
 #include <stdbool.h>
+#include "mainMenu.h"
 
-void drawPausMenuBackground() {
+void drawPausMenuBackground(PausMenu* menu) {
+
+    Vector2 origin = {0, 0};
+
+    DrawRectanglePro(menu->rects.background, origin, 0, menu->colors.background);
+    DrawRectanglePro(menu->rects.menuContainer, origin, 0, menu->colors.menu);
+}
+
+void initPausMenu(PausMenu* menu) {
 
     int menuWidth = 400;
     int menuHeight = 500;
+    int fontSize = 32;
+    int fontSpacing = 6;
+    int nextItemGap = 20;
+    
+    char titles[][32] = {
+        "RESUME GAME",
+        "OPTIONS",
+        "EXIT TO MENU",
+        "EXIT TO DESKTOP"
+    };
 
-    Rectangle background = {
+    int menuY = 0;
+
+    menu->rects.background = (Rectangle){
         0,
         0,
         SCREEN_WIDTH,
         SCREEN_HEIGHT
     };
 
-    Rectangle menuContainer = {
+    menu->rects.menuContainer = (Rectangle){
         (SCREEN_WIDTH / 2) - (menuWidth / 2),
         (SCREEN_HEIGHT / 2) - (menuHeight / 2),
         menuWidth,
         menuHeight
     };
 
-    Vector2 origin = {0, 0};
+    menu->colors.background = (Color) Fade(BLACK, 0.6f);
+    menu->colors.menu = (Color) Fade(DARKBLUE, 0.6f);
+    menu->colors.text = (Color) RAYWHITE;
 
-    Color backgroundColor = Fade(BLACK, 0.6f);
-    Color menuColor = Fade(DARKBLUE, 0.6f);
+    for (int i = 0; i < 4; i++) {
+        Vector2 size = MeasureTextEx(GetFontDefault(), titles[i], fontSize, fontSpacing);
 
-    DrawRectanglePro(background, origin, 0, backgroundColor);
-    DrawRectanglePro(menuContainer, origin, 0, menuColor);
+        menu->items[i].basePosition = (Vector2){
+            (SCREEN_WIDTH / 2) - (size.x / 2),
+            menuY
+        };
+
+        strcpy(menu->items[i].text, titles[i]);
+        menu->items[i].size = size;
+        menu->items[i].isHovered = false;
+
+        menuY += size.y + nextItemGap;
+    }
+
+    menu->count = 4;
+
+    int menuHeight = menuY - nextItemGap;
+    menu->menuOffset = (SCREEN_HEIGHT / 2) - (menuHeight / 2);
+
+    menu->selected = -1;
 }
--- pauseMenu.h
+++ pauseMenu.h
@@ -1,6 +1,34 @@
 #ifndef PAUSEMENU_H
 #define PAUSEMENU_H
 
+#include "raylib.h"
 
+typedef struct PausMenuItem {
+    char text[32];
+    Vector2 basePosition;
+    Vector2 size;
+    
+    bool isHovered;
+}PausMenuItem;
+
+typedef struct PausMenuRects {
+    Rectangle background;
+    Rectangle menuContainer;
+}PausMenuRects;
+
+typedef struct PauseMenuColors {
+    Color background;
+    Color menu;
+    Color text;
+}PauseMenuColors;
+
+typedef struct PausMenu {
+    PausMenuItem items[4];
+    int count;
+    int selected;
+    int menuOffset;
+    PausMenuRects rects;
+    PauseMenuColors colors;
+}PausMenu; 
 
 #endif
