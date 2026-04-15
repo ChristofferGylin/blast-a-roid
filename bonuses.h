e8d926a6590d61db87b8fe149202567496611980
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-15 22:19:59 +0200

    Created BonusBase, BonusMultiplier and Bonuses structs

--- bonuses.h
+++ bonuses.h
@@ -1,6 +1,23 @@
 #ifndef BONUSES_H
 #define BONUSES_H
 
+#include <stdbool.h>
+#include "raylib.h"
 
+typedef struct BonusBase {
+    bool isActive;
+    float spawnTime;
+    Vector2 position;
+}BonusBase;
+
+typedef struct BonusMultiplier {
+    BonusBase base;
+    int level;
+}BonusMultiplier;
+
+typedef struct Bonuses {
+    float nextSpawnTime;
+    BonusMultiplier bonusMultiplier;
+}Bonuses; 
 
 #endif
