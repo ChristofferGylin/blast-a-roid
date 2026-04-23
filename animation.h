88b48f9d4a121a4bf019a6e2beeeb40dcbbe1c29
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-23 18:33:09 +0200

    Created Frame and Animation structs

--- animation.h
+++ animation.h
@@ -1,4 +1,19 @@
 #ifndef ANIMATION
 #define ANIMATION
 
+#include "raylib.h"
+
+typedef struct Frame {
+    Rectangle rect;
+}Frame;
+
+typedef struct Animation {
+    Texture2D texture;
+    Frame* frames;
+    int frameCount;
+    int currentFrame;
+    double startTime;
+    float fps;
+}Animation;
+
 #endif
