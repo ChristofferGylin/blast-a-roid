f8373dbee8f9e1b2b219b45588c096367be26d8f
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-23 20:45:22 +0200

    Added position, isLoop and isFinished to Animation struct

--- animation.h
+++ animation.h
@@ -2,6 +2,7 @@
 #define ANIMATION
 
 #include "raylib.h"
+#include "stdbool.h"
 
 typedef struct Frame {
     Rectangle rect;
@@ -9,11 +10,14 @@ typedef struct Frame {
 
 typedef struct Animation {
     Texture2D texture;
+    Vector2 position;
     Frame* frames;
     int frameCount;
     int currentFrame;
     double startTime;
     float fps;
+    bool isLoop;
+    bool isFinished;
 }Animation;
 
 void initAnimation(Animation* animation, Texture2D spritesheet, const char* jsonPath, float fps);
