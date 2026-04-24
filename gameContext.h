6c7e0a7d7ed2a62fedf98f982102e5e14a9c1d06
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-25 00:09:30 +0200

    Created ObjectPools struct

--- gameContext.h
+++ gameContext.h
@@ -2,10 +2,20 @@
 #define GAMECONTEXT_H
 
 #include "raylib.h"
+#include "asteroid.h"
+#include "shooting.h"
+#include "animation.h"
 
 typedef struct Animations {
     Texture2D explosion;
 }Animations;
+
+typedef struct ObjectPools {
+    AsteroidPool asteroids;
+    DestroyedAsteroidPool destroyedAsteroids;
+    ShotObjectPool shots;
+    AnimationPool explosions;
+}ObjectPools;
 
 typedef struct Samples {
     Sound explosion;
