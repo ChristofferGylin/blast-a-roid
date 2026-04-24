ce43530da86107e7aa9301887700471185502614
Christoffer Gylin <christoffer@christoffergylin.com>
2026-04-24 23:46:39 +0200

    Created Sprites struct

--- gameContext.h
+++ gameContext.h
@@ -1,4 +1,12 @@
 #ifndef GAMECONTEXT_H
 #define GAMECONTEXT_H
 
+#include "raylib.h"
+
+typedef struct Sprites {
+    Texture2D asteroid;
+    Texture2D ship;
+    Texture2D shot;
+}Sprites;
+
 #endif
