6601442a323f7809442db4fcc06206da016be613
Christoffer Gylin <christoffer@christoffergylin.com>
2026-05-01 00:32:58 +0200

    Created Enemy struct and EnemyType enum

--- enemies.h
+++ enemies.h
@@ -1,4 +1,19 @@
 #ifndef ENEMIES_H
 #define ENEMIES_H
 
+#include "raylib.h"
+typedef enum EnemyType {
+    UFO_1
+}EnemyType;
+
+typedef struct Enemy {
+    Vector2 position;
+    Vector2 destinaton;
+    float velocity;
+    float health;
+    double shotTime;
+    int shotCount;
+    EnemyType type;
+}Enemy;
+
 #endif
