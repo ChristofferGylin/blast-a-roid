c10d1a44c77b8de03aac3ef6edc5df3bcdd7de1c
Christoffer Gylin <christoffer@christoffergylin.com>
2026-05-01 01:15:35 +0200

    Added rotation, VisualType enum and Texture2D/Animation union in Enemy struct and changed init functions to match

--- enemies.c
+++ enemies.c
@@ -1,1 +1,36 @@
 #include "enemies.h"
+#include "constants.h"
+#include "gameContext.h"
+
+void initUfo1(GameContext* ctx, Enemy* enemy);
+
+void initEnemy(GameContext* ctx, Enemy* enemy, EnemyType type) {
+
+    enemy->health = 1.0f;
+    enemy->rotation = 0.0f;
+    enemy->shotCount = 0;
+    enemy->shotTime = GetTime();
+
+    switch (type)
+    {
+    case UFO_1:
+        initUfo1(ctx, enemy);
+        break;
+    
+    default:
+        break;
+    }
+}
+
+void initUfo1(GameContext* ctx, Enemy* enemy) {
+
+    float y = 50.0f;
+    float velocity = 100;
+
+    enemy->destinaton = (Vector2){SCREEN_WIDTH + 10, y};
+    enemy->position = (Vector2){-10, y};
+    enemy->type = UFO_1;
+    enemy->velocity = velocity;
+    enemy->visualType = VISUAL_ANIMATION;
+    enemy->animation = &ctx->assets.animations.ufo1;
+}
--- enemies.h
+++ enemies.h
@@ -2,18 +2,33 @@
 #define ENEMIES_H
 
 #include "raylib.h"
+
+typedef struct Animation Animation;
+
 typedef enum EnemyType {
     UFO_1
 }EnemyType;
 
+typedef enum VisualType {
+    VISUAL_SPRITE,
+    VISUAL_ANIMATION
+}VisualType;
+
 typedef struct Enemy {
     Vector2 position;
     Vector2 destinaton;
+    float rotation;
     float velocity;
     float health;
     double shotTime;
     int shotCount;
     EnemyType type;
+    VisualType visualType;
+    union {
+        Texture2D* sprite;
+        Animation* animation;
+    };
+
 }Enemy;
 
 #endif
