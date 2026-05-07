4c0ff1d52fe1d793dbaea464355d9d7c65ec23cd
Christoffer Gylin <christoffer@christoffergylin.com>
2026-05-07 22:43:19 +0200

    Moved VisualTypes struct to utils.h

--- utils.h
+++ utils.h
@@ -10,6 +10,11 @@ typedef struct FloatRange {
     float max;
 }FloatRange;
 
+typedef enum VisualType {
+    VISUAL_SPRITE,
+    VISUAL_ANIMATION
+}VisualType;
+
 float scaleFloat(float oldMin, float oldMax, float newMin, float newMax, float value);
 int getNumberOfAsteroids(int gameLevel);
 Vector2 getRandomVelocity(FloatRange range);
