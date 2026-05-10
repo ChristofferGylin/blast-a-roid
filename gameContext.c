48cf93af565c4472816d3d840518af9c0db734cd
Christoffer Gylin <christoffer@christoffergylin.com>
2026-05-10 03:16:38 +0200

    Added new icons to Sprites in GameContext struct and load/unloadAssets functions

--- gameContext.c
+++ gameContext.c
@@ -42,12 +42,16 @@ void loadAssets(GameContext* ctx) {
     ctx->assets.samples.shot = LoadSound("./assets/samples/laser_pew.wav");
 
     ctx->assets.sprites.asteroid = LoadTexture("./assets/asteroid.png");
+    ctx->assets.sprites.autoShotIcon = LoadTexture("./assets/auto_shot_icon.png");
     ctx->assets.sprites.destroyedShip1 = LoadTexture("./assets/ship_destroyed_1.png");
     ctx->assets.sprites.destroyedShip2 = LoadTexture("./assets/ship_destroyed_2.png");
     ctx->assets.sprites.destroyedShip3 = LoadTexture("./assets/ship_destroyed_3.png");
     ctx->assets.sprites.enemyShot1 = LoadTexture("./assets/enemy_shot_1.png");
+    ctx->assets.sprites.lockIcon = LoadTexture("./assets/lock_icon.png");
+    ctx->assets.sprites.longShotIcon = LoadTexture("./assets/long_shot_icon.png");
     ctx->assets.sprites.ship = LoadTexture("./assets/ship.png");
     ctx->assets.sprites.shot = LoadTexture("./assets/shot.png");
+    ctx->assets.sprites.stopIcon = LoadTexture("./assets/stop_icon.png");
     ctx->assets.sprites.triShotIcon = LoadTexture("./assets/tri_shot_icon.png");
 }
 
