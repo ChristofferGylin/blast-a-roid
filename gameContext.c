2c5ebb790250ac2dd18c068672f8961800659718
Christoffer Gylin <christoffer@christoffergylin.com>
2026-05-25 02:05:01 +0200

    Added UFO_3_RENDER_SIZE and UFO_3_COLLISION_SIZE to constants, added ufo3 to load/unloadAssets

--- gameContext.c
+++ gameContext.c
@@ -36,6 +36,7 @@ void loadAssets(GameContext* ctx) {
     initAnimation(&ctx->assets.animations.explosion, "./assets/explosion.png", "./assets/explosion.json", 24.0f, (Vector2){EXPLOSION_SIZE, EXPLOSION_SIZE}, false);
     initAnimation(&ctx->assets.animations.ufo1, "./assets/ufo1.png", "./assets/ufo1.json", 24.0f, (Vector2){UFO_1_SIZE, UFO_1_SIZE}, true);
     initAnimation(&ctx->assets.animations.ufo2, "./assets/ufo2.png", "./assets/ufo2.json", 24.0f, (Vector2){UFO_1_SIZE, UFO_1_SIZE}, true);
+    initAnimation(&ctx->assets.animations.ufo3, "./assets/ufo3.png", "./assets/ufo3.json", 24.0f, (Vector2){UFO_3_RENDER_SIZE, UFO_3_RENDER_SIZE}, true);
     
     ctx->assets.samples.explosion = LoadSound("./assets/samples/explosion.wav");
     ctx->assets.samples.multiplier_collect = LoadSound("./assets/samples/multiplier_collect.wav");
@@ -63,6 +64,7 @@ void unloadAssets(GameContext* ctx) {
     unloadAnimation(&ctx->assets.animations.explosion);
     unloadAnimation(&ctx->assets.animations.ufo1);
     unloadAnimation(&ctx->assets.animations.ufo2);
+    unloadAnimation(&ctx->assets.animations.ufo3);
 
     UnloadSound(ctx->assets.samples.explosion);
     UnloadSound(ctx->assets.samples.shot);
