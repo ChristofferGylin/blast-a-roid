8fd14dc8d6e255d22e64bcbf1eb1e3dbca871f3b
Christoffer Gylin <christoffer@christoffergylin.com>
2026-06-16 16:39:06 +0200

    Created Special struct and moved SpecialType to specials.h

--- specials.c
+++ specials.c
@@ -1,8 +1,1 @@
 #include "specials.h"
-
-typedef enum SpecialType {
-    MULTIPLIER,
-    COMET,
-    EXTRA_LIFE,
-    SUPERNOVA
-}SpecialType;
--- specials.h
+++ specials.h
@@ -1,4 +1,33 @@
 #ifndef SPECIALS_H
 #define SPECIALS_H
 
+#include "raylib.h"
+#include "utils.h"
+#include "animation.h"
+
+static const int NUMBER_OF_SPECIALS = 4;
+
+typedef enum SpecialType {
+    MULTIPLIER,
+    COMET,
+    EXTRA_LIFE,
+    SUPERNOVA
+}SpecialType;
+
+typedef struct Special {
+    SpecialType type;
+    double spawnTime;
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 size;
+    float rotation;
+    float rotationSpeed;
+    float value;
+    VisualType visualType;
+    union {
+        Texture2D* sprite;
+        AnimationInstance animation;
+    };
+}Special;
+
 #endif
