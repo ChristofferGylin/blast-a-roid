8a27a0778414ea9ad36ea176fdf920ef9ea6eeae
Christoffer Gylin <christoffer@christoffergylin.com>
2026-07-01 14:48:53 +0200

    Created new Highscore struct

--- score.h
+++ score.h
@@ -1,8 +1,16 @@
 #ifndef SCORE_H
 #define SCORE_H
 
+#include <inttypes.h>
+
 #include "asteroid.h"
 #include "player.h"
+
+typedef struct Highscore {
+    uint64_t score;
+    char name[32];
+    int level;
+}Highscore;
 
 
 void addScore(Player* player, Asteroid* ast);
