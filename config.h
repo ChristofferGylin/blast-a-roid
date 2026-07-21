576d001ea79fb1e279142beeb363b2a3023efcea
Christoffer Gylin <christoffer@christoffergylin.com>
2026-07-21 17:28:17 +0200

    Moved newly created structs to header file because stupid

--- config.h
+++ config.h
@@ -1,4 +1,38 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <inttypes.h>
+#include <stdbool.h>
+
+#include "debug.h"
+
+typedef struct ObjectPoolCountConfig {
+    bool asteroids;
+    bool bonuses;
+    bool spawnableBonuses;
+    bool destroyedAsteroids;
+    bool enemies;
+    bool spawnableEnemies;
+    bool explosions;
+    bool shots;
+    bool specials;
+    bool specialsSpawn;
+}ObjectPoolCountConfig;
+
+typedef struct DebugConfig {
+    bool onlyOutputOnChange;
+    float outputFrequency;
+    ObjectPoolCountConfig poolCount;
+}DebugConfig;
+
+typedef struct Config {
+    DebugConfig debug;
+}Config;
+
+typedef struct ConfigSaveData
+{
+    Config config;
+    uint32_t checksum;
+};
+
 #endif
