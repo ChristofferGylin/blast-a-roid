9d1efd1fc7c81459c23b44a3b4f0bbc7760257e8
Christoffer Gylin <christoffer@christoffergylin.com>
2026-07-22 14:01:59 +0200

    Created OptionsMenu and OptionsMenuTab structs

--- optionsMenu.h
+++ optionsMenu.h
@@ -1,4 +1,22 @@
 #ifndef OPTIONSMENU_H
 #define OPTIONSMENU_H
 
+#include "ui.h"
+
+#define NUMBER_OF_OPTIONS_TABS 3
+
+typedef void (*DrawOptionsTab)(void* userData);
+
+typedef struct OptionsMenuTab {
+    char heading[TITLE_MAX_LENGTH];
+    DrawOptionsTab drawTab;
+    void *userData;
+}OptionsMenuTab;
+
+typedef struct OptionsMenu {
+    BasicLayoutContainer layout;
+    Button backButton;
+    OptionsMenuTab tabs[NUMBER_OF_OPTIONS_TABS];
+}OptionsMenu;
+
 #endif
