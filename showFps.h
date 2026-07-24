a31267cca523e837334a904cd45dbc2be037a37c
Christoffer Gylin <christoffer@christoffergylin.com>
2026-07-24 13:00:01 +0200

    Changed FPS_VALUE_FONT_SIZE to 72

--- showFps.h
+++ showFps.h
@@ -8,7 +8,7 @@ typedef struct GameContext GameContext;
 static const int FPS_TITLE_FONT_SIZE = 18;
 static const int FPS_TITLE_FONT_SPACING = 8;
 static const int FPS_CURRENT_VALUE_FONT_SIZE = 24;
-static const int FPS_VALUE_FONT_SIZE = 12;
+static const int FPS_VALUE_FONT_SIZE = 72;
 static const int FPS_GAP = 8;
 
 typedef struct RenderFpsPositions {
