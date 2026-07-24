04b1a893194e0dbaf3e87481b0b6f0be98cf1aa6
Christoffer Gylin <christoffer@christoffergylin.com>
2026-07-24 12:26:46 +0200

    Created fontSizeMeasureTool to help calculate optimal font relativity

---
+++ fontSizeMeasureTool.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#include "showFps.h"
+#include "raylib.h"
+
+int main() {
+
+    InitWindow(800, 600, "Test");
+
+    int finalSize = 0;
+
+    int fontSize = 12;
+
+    while(finalSize == 0) {
+        Vector2 titleSize = MeasureTextEx(GetFontDefault(), "CURRENT", FPS_TITLE_FONT_SIZE, FPS_TITLE_FONT_SPACING);
+        Vector2 valueSize = MeasureTextEx(GetFontDefault(), "999", fontSize, FPS_TITLE_FONT_SPACING);
+
+        if (valueSize.x > titleSize.x) {
+            finalSize = fontSize - 1;
+        } else {
+            fontSize++;
+        }
+    }
+
+    printf("\nFinal font size: %d\n", finalSize);
+
+    CloseWindow();
+
+    return 0;
+}
