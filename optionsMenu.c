69bcaf16a6b7d317ba6b22cf0f7c11aff26e712e
Christoffer Gylin <christoffer@christoffergylin.com>
2026-07-29 17:34:04 +0200

    Restored testing stuff

--- optionsMenu.c
+++ optionsMenu.c
@@ -10,7 +10,7 @@
 #include "ui.h"
 #include "uiSizes.h"
 
-void drawOptionsMenu(OptionsMenu* menu, DropdownMenu* dropdownMenu);
+void drawOptionsMenu(OptionsMenu* menu);
 void drawOptionsMenuTab(OptionsMenu* menu);
 void initOptionsMenu(GameContext* ctx, OptionsMenu* menu);
 void initOptionsMenuTab(OptionsMenuTab* tab, Rectangle* parent, char* heading, Callback drawContent, Callback updateTab, void* userData);
@@ -126,12 +126,11 @@ void initVideoTabData(GameContext* ctx, Rectangle* parent, VideoTabData* tabData
     tabData->ctx = ctx;
 }
 
-void drawOptionsMenu(OptionsMenu* menu, DropdownMenu* dropdownMenu) {
+void drawOptionsMenu(OptionsMenu* menu) {
     BeginDrawing();
         drawBasicLayoutContainer(&menu->layout);
         drawButton(&menu->backButton);
         drawOptionsMenuTab(menu);
-        drawDropdownMenu(dropdownMenu);
     EndDrawing();
 }
 
@@ -169,32 +168,11 @@ void drawVideoTab(void* userData) {
     }
 }
 
-void dropdownCallback(int index, void* userData) {
-    printf("\n%d\n", index);
-}
-
 bool optionsMenu(GameContext* ctx) {
 
     Config initialConfigState = getConfig(ctx);
     
     OptionsMenu menu;
-
-    DropDownTitles dropdownTitles = {
-        "Item 1565674636i7456865757",
-        "Item 2",
-        "Item 3",
-    };
-
-    DropdownMenu dropdownMenu;
-    initDropdownMenu(
-        &dropdownMenu,
-        dropdownTitles,
-        3,
-        0,
-        (Rectangle){SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f, 150, 0},
-        dropdownCallback,
-        NULL
-    );
 
     initOptionsMenu(ctx, &menu);
 
@@ -202,8 +180,7 @@ bool optionsMenu(GameContext* ctx) {
 
     while (!WindowShouldClose()) {
         updateOptionsMenu(&menu);
-        updateDropdownMenu(&dropdownMenu);
-        drawOptionsMenu(&menu, &dropdownMenu);
+        drawOptionsMenu(&menu);
 
         if (menu.exit) break;
     }
