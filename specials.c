bfda80ee0d530cdefdc3f0e60764f7b6a402d8ac
Christoffer <christoffer@christoffergylin.com>
2026-08-19 18:04:52 +0200

    Restored testing stuff

--- specials.c
+++ specials.c
@@ -413,10 +413,10 @@ void initSpecialsSpawnPool(GameContext* ctx) {
 void populateSpecialsSpawnPool(GameContext* ctx) {
     
     SpecialSpawnOption optionPool[NUMBER_OF_SPECIALS] = {
-        // (SpecialSpawnOption){true, MULTIPLIER, 100},
-        // (SpecialSpawnOption){true, COMET, 100},
-        // (SpecialSpawnOption){true, BLACK_HOLE, 30},
-        // (SpecialSpawnOption){true, SUPERNOVA, 20},
+        (SpecialSpawnOption){true, MULTIPLIER, 100},
+        (SpecialSpawnOption){true, COMET, 100},
+        (SpecialSpawnOption){true, BLACK_HOLE, 30},
+        (SpecialSpawnOption){true, SUPERNOVA, 20},
         (SpecialSpawnOption){true, EXTRA_LIFE, 20},
     };
     SpecialsSpawnPool* spawnPool = &ctx->objectPools.specialsSpawn;
@@ -426,7 +426,7 @@ void populateSpecialsSpawnPool(GameContext* ctx) {
 
     if (maxNumberOfSpecials > NUMBER_OF_SPECIALS) maxNumberOfSpecials = NUMBER_OF_SPECIALS;
 
-    int numberToPopulate = 1; // GetRandomValue(minNumberOfSpecials, maxNumberOfSpecials);
+    int numberToPopulate = GetRandomValue(minNumberOfSpecials, maxNumberOfSpecials);
 
     for (int i = 0; i < numberToPopulate; i++) {
         
